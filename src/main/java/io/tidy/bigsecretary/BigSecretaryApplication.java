ad6b27dd5d87998a80e1647a863133b77be05110
bebo0 <bebo007@naver.com>
2025-02-17 22:32:04 +0900

    Init Project

---
+++ src/main/java/io/tidy/bigsecretary/BigSecretaryApplication.java
@@ -0,0 +1,13 @@
+package io.tidy.bigsecretary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigSecretaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigSecretaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tidy/bigsecretary/BigSecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tidy.bigsecretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigSecretaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
